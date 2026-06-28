1c88197f6a95093a3edee775ab3f128ea9460a5a
Moritz Hemmann <49557417+M00He@users.noreply.github.com>
2026-06-28 09:58:59 +0200

    feat(app): reine Hilfsfunktionen fuer Backup-Dateiname und Restore-Vorschau

---
+++ functions/app_helpers.R
@@ -0,0 +1,33 @@
+# App-Hilfsfunktionen (rein, ohne Shiny)
+
+safe_filename <- function(s) {
+  s <- gsub("[^[:alnum:]_-]", "_", s)
+  s <- gsub("_+", "_", s)
+  s <- gsub("^_|_$", "", s)
+  if (s == "") "turnier" else s
+}
+
+backup_filename <- function(state) {
+  base <- if (is.null(state$tournament_name) || state$tournament_name == "") {
+    "turnier"
+  } else {
+    safe_filename(state$tournament_name)
+  }
+  paste0(base, "_runde", state$current_round, ".json")
+}
+
+state_summary <- function(state) {
+  status_label <- switch(state$status,
+    setup = "Noch nicht gestartet",
+    running = "Läuft",
+    finished = "Abgeschlossen",
+    state$status)
+  name <- if (is.null(state$tournament_name) || state$tournament_name == "") {
+    "(ohne Namen)"
+  } else {
+    state$tournament_name
+  }
+  list(name = name, round = state$current_round,
+       num_rounds = state$settings$num_rounds,
+       n_players = nrow(state$players), status_label = status_label)
+}
---
+++ tests/testthat/test-app_helpers.R
@@ -0,0 +1,26 @@
+source("../../functions/tournament_state.R", encoding = "UTF-8")
+source("../../functions/game_system.R", encoding = "UTF-8")
+source("../../functions/app_helpers.R", encoding = "UTF-8")
+
+test_that("safe_filename säubert Strings", {
+  expect_equal(safe_filename("Vereins Turnier 2026!"), "Vereins_Turnier_2026")
+  expect_equal(safe_filename(""), "turnier")
+  expect_equal(safe_filename("a/b\\c"), "a_b_c")
+})
+
+test_that("backup_filename nutzt Name und Runde", {
+  s <- new_tournament_state(name = "Sommer Cup"); s$current_round <- 3L
+  expect_equal(backup_filename(s), "Sommer_Cup_runde3.json")
+  expect_equal(backup_filename(new_tournament_state()), "turnier_runde1.json")
+})
+
+test_that("state_summary liefert Vorschau-Felder", {
+  s <- new_tournament_state(name = "X")
+  for (i in 1:4) s <- ts_add_player(s, paste("P", i), "m")
+  s <- ts_start_tournament(s, 5L, 2L, "best_of_3_11")
+  summ <- state_summary(s)
+  expect_equal(summ$name, "X")
+  expect_equal(summ$n_players, 4L)
+  expect_equal(summ$num_rounds, 5L)
+  expect_equal(summ$status_label, "Läuft")
+})
